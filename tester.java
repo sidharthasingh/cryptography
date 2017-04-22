9ccf173ed9046787c60d3f36b203f660c108a0fe
Sidhartha Singh <Sidharthasingh2@gmail.com>
2017-04-22 14:42:01 +0530

    class : NUMBER : added subtraction method

--- tester.java
+++ tester.java
@@ -2,12 +2,14 @@ class tester
 {
 	public static void main(String arg[])
 	{
-		System.out.println(binary.toBinary(256));
-		System.out.println(binary.fromBinary("100000"));
-		System.out.println(binary.xor("101110110","10101"));
-		System.out.println(binary.fromBinary(binary.add(binary.toBinary(12078),binary.toBinary(22))));
-		System.out.println(binary.rightShift("1011"));
-		System.out.println(binary.isBinary("10101010101010a100010101010101"));
-		System.out.println(binary.equalizeLength("101101101"));
+		Number n=new Number(2);
+		try
+		{
+			System.out.println(Number.sub(new Number(1000),new Number(998)).retNum());
+		}
+		catch(Exception e)
+		{
+
+		}
 	}
 }
